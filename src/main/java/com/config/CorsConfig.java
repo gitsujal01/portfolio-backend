5e1a66d0a2fc822aa064613872547ebe48e15921
gitsujal01 <sujallokhande23@gmail.com>
2026-03-04 23:42:40 +0530

    added origin

--- src/main/java/com/config/CorsConfig.java
+++ src/main/java/com/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://www.sujallokhande.in") // frontend URL
+                .allowedOrigins("https://www.sujallokhande.in")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/com/mylibrary/controller/EmailController.java
+++ src/main/java/com/mylibrary/controller/EmailController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.mylibrary.services.EmailJsService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class EmailController {
 
     @Autowired
